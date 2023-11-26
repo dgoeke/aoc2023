fd6b401c4a144e5ceb2243dca9bbfe6033a07eb3
David Goeke <dg@corvid.local>
2023-11-26 11:02:33 -0800

    base

---
+++ src/aoc2023/core.clj
@@ -0,0 +1,19 @@
+(ns aoc2023.core
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str])
+  (:gen-class))
+
+(defn- load-resource [filename]
+  (some->> filename io/resource slurp))
+
+(defn day
+  "Load input data from resources directory. Returns `nil`
+   if file does not exist, or a string if it does.
+    (day 1)                => day1.txt
+    (day 1 :sample)        => day1-sample.txt
+    (day 2 :sample :part2) => day2-sample-part2.txt"
+  [n & args]
+  (load-resource
+    (str (str/join "-" (cons (str "day" n)
+                             (map name args)))
+         ".txt")))
